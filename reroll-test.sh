854298c771c3b75a783bc374e59e90690bb798e3
Jesper Mathiassen <jm@bellcom.dk>
2013-03-04 16:15:49 +0100

    Test now makes a dir in build

--- reroll-test.sh
+++ reroll-test.sh
@@ -3,4 +3,4 @@
 #export http_proxy=http://localhost:3128/
 #export ftp_proxy=http://localhost:3128/
 
-./os2web_build.py -m profile -f os2web.dev.make -t os2web-test-`date +%Y%m%d%H%M`
+./os2web_build.py -m profile -f os2web.dev.make os2web-test-`date +%Y%m%d%H%M`
